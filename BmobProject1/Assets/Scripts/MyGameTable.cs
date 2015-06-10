62bf62adbac1c84e983e17addd21252dbd9b1d19
wuzhangwuzhang <930116897@qq.com>
2015-06-10 15:52:12 +0800

    Unity Bmob云存储.
    Signed-off-by: wuzhangwuzhang <930116897@qq.com>

---
+++ BmobProject1/Assets/Scripts/MyGameTable.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using cn.bmob.io;
+
+public class MyGameTable : BmobTable
+{
+    /// <summary>
+    /// Bmob服务器端我们定义的表名
+    /// </summary>
+    public const string TABLENAME = "MyGameTable";
+
+    /// <summary>
+    /// 玩家姓名
+    /// </summary>
+    public string playername { get; set; }
+    /// <summary>
+    /// 玩家得分
+    /// </summary>
+    public BmobInt score { get; set; }
+
+    /// <summary>
+    /// 成员函数
+    /// </summary>
+    /// <param name="input"></param>
+    public override void readFields(BmobInput input)
+    {
+        base.readFields(input);
+        //读取相应的字段
+        this.score = input.getInt("score");
+        this.playername = input.getString("playername");
+
+    }  
+    public override void write(BmobOutput output, bool all)
+    {
+        base.write(output, all);
+        //写入到数据库对应的字段
+        output.Put("score", this.score);
+        output.Put("playername", this.playername);
+    }
+}
---
+++ BmobProject1/Assets/Scripts/TestBmob1.cs
@@ -0,0 +1,145 @@
+﻿using UnityEngine;
+using System.Collections;
+using cn.bmob.api;
+using cn.bmob.tools;
+using cn.bmob.io;
+using System.Collections.Generic;
+
+public class TestBmob1 : MonoBehaviour
+{
+    private BmobUnity Bmob;
+
+    // Use this for initialization
+    void Start()
+    {
+        BmobDebug.Register(print);
+        Bmob = gameObject.GetComponent<BmobUnity>();
+    }
+
+    void OnGUI()
+    {
+        if (GUILayout.Button("Insert"))
+        {
+            InsertData();
+        }
+        if (GUILayout.Button("GetData"))
+        {
+            getRecoard();
+        }
+        if (GUILayout.Button("Update"))
+        {
+            updateData();
+        }
+        if (GUILayout.Button("AllData"))
+        {
+            getAllInfo();
+        }
+        if (GUILayout.Button("删除数据"))
+        {
+            deleteData();
+        }
+    }
+
+    #region
+    /// <summary>
+    /// 插入数据
+    /// </summary>
+    public void InsertData()
+    {
+        MyGameTable mg = new MyGameTable();
+        mg.score = 100;
+        mg.playername = "testBmob";
+        
+
+        Bmob.Create(MyGameTable.TABLENAME, mg, (resp, exception) =>
+        {
+            if (exception != null)
+            {
+                Debug.Log("保存失败，原因： " + exception.Message);
+            }
+            else
+            {
+                Debug.Log("保存成功" + resp.createdAt);
+            }
+        });
+    }
+    /// <summary>
+    /// 获取表所以信息
+    /// </summary>
+    public void getAllInfo()
+    {
+        	BmobQuery query = new BmobQuery();
+            query.WhereEqualTo("playername", "testBmob");
+            Bmob.Find<MyGameTable>(MyGameTable.TABLENAME, query, (resp, exception) =>
+            {
+                if (exception != null)
+                {
+                    print("查询失败, 失败原因为： " + exception.Message);
+                    return;
+                }
+
+                //对返回结果进行处理
+                List<MyGameTable> list = resp.results;
+                for (int i = 0; i < list.Count; i++)
+                {
+                    print(list[i].playername.ToString() + "\t" + list[i].score.ToString() + "\t" + list[i].updatedAt.ToString());
+                }
+            });
+    }   
+    /// <summary>
+    /// 查询数据
+    /// </summary>
+    public void getRecoard()
+    {
+        MyGameTable mg = new MyGameTable();
+
+        Bmob.Get<MyGameTable>(MyGameTable.TABLENAME, "674dcc697d", (resp, exception) =>
+        {
+            if (exception != null)
+            {
+                Debug.Log("查询失败, 失败原因为： " + exception.Message);
+                return;
+            }
+
+            MyGameTable game = resp;
+            Debug.Log("playerName:"+game.playername + "\n"+"Score:" + game.score );
+        });
+    }
+    /// <summary>
+    /// 更新数据
+    /// </summary>
+    public void updateData()
+    {
+        MyGameTable game = new MyGameTable();
+        game.playername = "wuzhang";
+        Bmob.Update(MyGameTable.TABLENAME, "674dcc697d", game, (resp, exception) =>
+        {
+            if (exception != null)
+            {
+                Debug.Log("保存失败, 失败原因为： " + exception.Message);
+                return;
+            }
+
+            Debug.Log("保存成功, @" + resp.updatedAt);
+        });
+    }
+    /// <summary>
+    /// 删除数据
+    /// </summary>
+    public void deleteData()
+    {
+        Bmob.Delete(MyGameTable.TABLENAME, "674dcc697d", (resp, exception) =>
+        {
+            if (exception != null)
+            {
+                Debug.Log("删除失败, 失败原因为： " + exception.Message);
+                return;
+            }
+            else
+            {
+                Debug.Log("删除成功, @" + resp.msg);
+            }
+        });
+    }
+    #endregion
+}
